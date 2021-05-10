024af6ad94b5d1bfb3952e1843e938e85c70f29b
Nakhat-Yemen <nakhatyemen.syeda@shinesolutions.com>
2021-05-10 16:29:55 +1000

    Added api basic

--- src/Components/ContentSection/index.js
+++ src/Components/ContentSection/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import styles from "./ContentSection.module.css";
+import axios from "axios";
 
 export default function ContentSection() {
+
+  const url = ' https://challenge.lexicondigital.com.au/api/v2/filmworld/movies'
+  const config = {
+  headers: {
+    'x-api-key': 'Yr2636E6BTD3UCdleMkf7UEdqKnd9n361TQL9An7',
+    }
+  }
+
+  const [data , setData] = useState(null);
+
+  useEffect(()=>{
+    axios.get(url, config).then(response => {
+    setData(response.data)
+  })
+  }, []);
+
+
   return (
-    <div className={styles.contentSection}>
+   data ?
+     <div className={styles.contentSection}>
+      {  JSON.stringify(data) }
     </div>
+       :
+       <div>
+         Nothing to display
+       </div>
   );
 }
