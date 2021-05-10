4e7f93cecde525b19d1f326b1364bb46c550fac6
Nakhat-Yemen <nakhatyemen.syeda@shinesolutions.com>
2021-05-10 14:24:31 +1000

    updated header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div>
+      <div className={styles.heading}>Prince's Theatre</div>
+      <div className={styles.subHeading}>Classic Movies At Home</div>
+      <div className={styles.subHeadingContent}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation
+      </div>
+    </div>
+  );
+}
